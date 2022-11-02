752d878f769762d67472d1d42b2a90a927351bcc
ymadike <m.yade@isepdiamniadio.sn>
2022-11-02 22:45:32 +0000

    My fourth

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,1 +1,1 @@
-curl 'https://learn.zone01dakar.sn/assets/superhero/all.json' | jq '.[70].name' | cat -e
+curl https://learn.zone01dakar.sn/assets/superhero/all.json | jq ' .[] | select( .id == 70 )| .name'
